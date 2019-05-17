23025a00bf7bf776c48f4a150f9f48ed9bef7ff3
Onur Tekin <30865800+csufeardir@users.noreply.github.com>
2019-05-17 05:06:30 +0200

    Add files via upload

---
+++ src/components/Stop_Component/Stop.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Typography from '@material-ui/core/Typography';
+
+class Stop extends Component {
+
+    render(){
+        return(
+            <div>
+                <Typography variant='display3'>I think this is all of it mate :/<br/>Wanna try a new one?</Typography>
+            </div>
+        )
+    }
+
+}
+
+export default Stop;
