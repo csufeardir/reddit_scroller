6bcf997f0e21f77a8d74b93ac3fe4f85dde46e98
Onur Tekin <30865800+csufeardir@users.noreply.github.com>
2019-05-18 19:08:46 +0200

    Update Appbar.js

--- src/components/Appbar_Component/Appbar.js
+++ src/components/Appbar_Component/Appbar.js
@@ -1,80 +1,50 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
-import CssBaseline from '@material-ui/core/CssBaseline';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import Paper from '@material-ui/core/Paper';
-import Fab from '@material-ui/core/Fab';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-import ListSubheader from '@material-ui/core/ListSubheader';
-import Avatar from '@material-ui/core/Avatar';
-import MenuIcon from '@material-ui/icons/Menu';
-import AddIcon from '@material-ui/icons/Add';
-import SearchIcon from '@material-ui/icons/Search';
-import MoreIcon from '@material-ui/icons/MoreVert';
-import ArrowUp from '@material-ui/icons/ArrowUpward';
+import Menu from '../Menu_Component/Menu'
 
-const styles = theme => ({
-  text: {
-    paddingTop: theme.spacing.unit * 2,
-    paddingLeft: theme.spacing.unit * 2,
-    paddingRight: theme.spacing.unit * 2,
+const styles = {
+  root: {
+    flexGrow: 1,
   },
-  paper: {
-    paddingBottom: 50,
+  grow: {
+    flexGrow: 1,
   },
-  list: {
-    marginBottom: theme.spacing.unit * 2,
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
   },
-  subHeader: {
-    backgroundColor: theme.palette.background.paper,
-  },
-  appBar: {
-    top: 'auto',
-    bottom: 0,
-  },
-  toolbar: {
-    alignItems: 'center',
-    justifyContent: 'space-between',
-  },
-  fabButton: {
-    position: 'absolute',
-    zIndex: 1,
-    top: -30,
-    left: 0,
-    right: 0,
-    margin: '0 auto',
-  },
-});
+};
+
+const aStyle ={
+    color:'inherit',
+    textDecoration:'none'
+}
 
 
+const profile='https://www.reddit.com/u/csufeardir'
 
-function BottomAppBar(props) {
+function ButtonAppBar(props) {
   const { classes } = props;
   return (
-    <React.Fragment>
-      <CssBaseline />
-      <AppBar position="fixed" color="white" className={classes.appBar}>
-        <Toolbar className={classes.toolbar}>
-          <Fab color="secondary" aria-label="Add" className={classes.fabButton}>
-            <ArrowUp onClick={window.scrollTop=0}/>
-          </Fab>
-          <Typography variant="h6" color="inherit">
-            Reddit Scroller!
+    <div className={classes.root}>
+      <AppBar position="fixed" className='AppBar' style={{background: 'transparent', boxShadow: 'none'}}>
+        <Toolbar>
+            <Menu/>
+          <Typography variant="h6" color="black" className={classes.grow}>
+            Reddit-Scroller!
           </Typography>
         </Toolbar>
       </AppBar>
-    </React.Fragment>
+    </div>
   );
 }
 
-BottomAppBar.propTypes = {
+ButtonAppBar.propTypes = {
   classes: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles)(BottomAppBar);
+export default withStyles(styles)(ButtonAppBar);
