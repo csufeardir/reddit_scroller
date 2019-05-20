bbc819b7a6346dacb096100c4e78bbfed2baccce
Onur Tekin <30865800+csufeardir@users.noreply.github.com>
2019-05-20 14:13:04 +0200

    Update Appbar.js

--- src/components/Appbar_Component/Appbar.js
+++ src/components/Appbar_Component/Appbar.js
@@ -43,7 +43,48 @@ function ButtonAppBar(props) {
   );
 }
 
+ButtonAppBar.propTypes = {import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Menu from '../Menu_Component/Menu'
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+};
+
+function ButtonAppBar(props) {
+  const { classes } = props;
+  return (
+    <div className={classes.root}>
+      <AppBar position="fixed" className='AppBar' style={{background: '#FFFFFF', boxShadow: 'none'}}>
+        <Toolbar>
+            <Menu/>
+          <Typography variant="h6" color="black" className={classes.grow}>
+            Reddit-Scroller!
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
 ButtonAppBar.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(ButtonAppBar);
   classes: PropTypes.object.isRequired,
 };
 
